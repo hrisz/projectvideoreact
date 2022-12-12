9e925b86418020f326187178e8b7e29f9f244de0
hrisz <harissaefuloh@gmail.com>
2022-12-12 21:24:41 +0700

    inihasilreact

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://softprodigy.com/wp-content/uploads/2019/06/react-js.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          LATIHAN REACT
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://youtube.com/c/HrisZ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ini youtube channelku
         </a>
       </header>
     </div>
